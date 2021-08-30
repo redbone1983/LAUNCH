9fdf31a59f707004f775ae27eae47c8cd58ab998
Maya Roman <maya.roman7@gmail.com>
2021-08-30 14:40:10 -0500

    Initial Commit

---
+++ JS_101/Lesson_2/rock_paper_scissors.js
@@ -0,0 +1,17 @@
+const readline = require('readline-sync');
+
+/*
+ * GAMES RULES
+ * 1. If player a chooses rock and player b chooses scissors, player a wins.
+ * 2. If player a chooses paper and player b chooses rock, player a wins.
+ * 3. If player a chooses scissors and player b chooses paper, player a wins.
+ * 4. If both players choose the same item, neither player wins. It's a tie.
+ */
+
+// GAME FLOW
+
+/*
+ * 1. The user makes a choice.
+ * 2. The computer makes a choice.
+ * 3. The winner is displayed.
+ */
