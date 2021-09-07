6c41ef26600699572baadc5faf66c0834d9001f5
Maya Roman <maya.roman7@gmail.com>
2021-09-07 14:28:42 -0500

    Add spock and lizard weapons

--- JS_101/Lesson_2/rock_paper_scissors.js
+++ JS_101/Lesson_2/rock_paper_scissors.js
@@ -1,19 +1,32 @@
+/* eslint-disable no-useless-catch */
 /* eslint-disable no-console */
 const readline = require('readline-sync');
 
 let choices = '';
+let gameCount = 1;
+let humanPoints = 0;
+let computerPoints = 0;
+let champion = false;
 
 // Set up game constants
 const VALID_CHOICES = [
   'r', 
   'p', 
-  's'
+  's',
+  'sp',
+  'l'
 ];
 
 const wins = [
   'sp',
   'pr',
-  'rs'
+  'rs',
+  'rl',
+  'sps',
+  'sl',
+  'lp',
+  'psp',
+  'spr'
 ];
 
 const randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
@@ -26,46 +39,89 @@ const displayWinner = (human, computer) => {
   choices += human;
   choices += computer;
 
-  if (wins.includes(choices)) {
-    return 'Human wins!';
-  } else if (human === computer) {
-    return 'You have tied!';
-  } 
+  if (human === computer) {
+    return 'IT IS A TIE!';
+  } else if (wins.includes(choices)) {
+    humanPoints += 1;
     
-return 'Computer wins!';
-  
+    return 'HUMAN WINS!';
+  } else if (!wins.includes(choices) && human !== computer && human !== 'x') {
+    computerPoints += 1;
+    
+    return 'COMPUTER WINS!';
+  }
 };
+
+let greeting = '*******************************************\nWELCOME TO ROCK, PAPER, SCISSORS, SPOCK, LIZARD!\n*******************************************\nBest out of 5 games is crowned Champion!\n*******************************************';
+
+const gameGreeting = msg => console.log(msg);
 
 // // Start Main Game Loop
 while (playGame) {
+  if (gameCount === 1) {
+    gameGreeting(greeting);
+  }
 
-  prompt('Please choose: p for paper, r for rock, s for scissors.');
-  let humanChoice = readline.question();
+  prompt(`GAME ${gameCount} STARTS NOW`);
+  prompt('*******************************************');
+  prompt('Please choose: r for rock, p for paper, s for scissors, sp for spock, l for lizard or x to exit game.');
+  
+  let humanChoice = readline.question().toLowerCase();
   let computerChoice = VALID_CHOICES[randomIndex];
+  console.clear();
+
+  if (humanChoice.toLowerCase() === 'x') {
+    break;
+  }
 
     // Start validation loop
     while (true) {
       if (VALID_CHOICES.includes(humanChoice)) {
+        gameCount += 1;
+        console.clear();
         break;
       } else {
         prompt("Please enter a valid input.");
+        humanChoice = readline.question().toLowerCase();
       }
-    }
-
+  }
+  
   prompt(`You chose ${humanChoice}, computer chose ${computerChoice}`);
 
+  prompt('*******************************************');
   prompt(displayWinner(humanChoice, computerChoice));
+  choices = '';
 
-  prompt('Would you like to play again? (y/n)')
-  let answer = readline.question().toLowerCase();
+  if (gameCount === 6 && humanPoints > computerPoints) {
+    champion = true;
+    prompt("Human reigns supreme!");
+    gameCount = 1;
+  } else if (gameCount === 6 && computerPoints > humanPoints) {
+    champion = true;
+    prompt("Computer reigns supreme!");
+    gameCount = 1;
+  } 
+
+  prompt('*******************************************');
+  prompt(`Current Score is You: ${humanPoints} & Computer: ${computerPoints}.`);
+  prompt('*******************************************');
+
   
-  while (answer[0] !== 'n' && answer[0] !== 'y') {
-    prompt('Please enter "y" or "n".');
-    answer = readline.question().toLowerCase();
-  }
+  if (champion) {
+    prompt('Would you like to play again? (y/n)');
+    let answer = readline.question().toLowerCase();
 
-  // eslint-disable-next-line curly
-  if (answer[0] !== 'y') {
-    playGame = false;
-  }
+    while (answer[0] !== 'n' && answer[0] !== 'y') {
+      prompt('Please enter "y" or "n".');
+      answer = readline.question().toLowerCase();
+    }
+
+    if (answer[0] === 'y') {
+      champion = false;
+      humanPoints = 0;
+      computerPoints = 0;
+    } else {
+      playGame = false;
+    }
+  } 
 }
