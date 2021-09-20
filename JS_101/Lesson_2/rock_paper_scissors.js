f9237a15ad79b3acedce22bcfe694bee2456eb94
Maya Roman <maya.roman7@gmail.com>
2021-09-20 14:57:51 -0500

    Code Review Refactor Part 2

--- JS_101/Lesson_2/rock_paper_scissors.js
+++ JS_101/Lesson_2/rock_paper_scissors.js
@@ -32,8 +32,7 @@ const VALID_CHOICES = [
   'p', 
   's',
   'sp',
-  'l',
-  'x'
+  'l'
 ];
 
 const WINS = {
@@ -58,7 +57,7 @@ const WEAPONS = {
 };
 
 let greeting = `WELCOME TO ROCK, PAPER, SCISSORS, SPOCK, LIZARD!`;
-let pleaseChoose = `Please choose: "r" for ROCK, "p" for PAPER, "s" for SCISSORS, "sp" for SPOCK, "l" for LIZARD or "x" to EXIT GAME`;
+let pleaseChoose = `Please choose: "r" for ROCK, "p" for PAPER, "s" for SCISSORS, "sp" for SPOCK, "l" for LIZARD`;
 let shortRules = `BEST OUT OF 5 ROUNDS IS CROWNED CHAMPION!`;
 let gameRulesPrompt = `Press "Y" for game rules. Press "N" to continue game.`;
 let longRules = ` SCISSORS CUTS PAPER\n PAPER COVERS ROCK\n ROCK CRUSHES LIZARD\n LIZARD POISONS SPOCK\n SPOCK SMASHES SCISSORS\n SCISSORS DECAPITATES LIZARD\n LIZARD EATS PAPER\n PAPER DISPROVES SPOCK\n SPOCK VAPORIZES ROCK\n ROCK CRUSHES SCISSORS`;
@@ -127,48 +126,30 @@ const getComputerChoice = () => {
 };
 
 const determineWinner = (player1, player2) => {
-  let winner = '';
   GAME.choices += player1;
   GAME.choices += player2;
 
   if (player1 === player2) {
-    winner = 'tie';
+    return 'tie';
   } 
   
   if (Object.keys(WINS).includes(GAME.choices)) {
-    winner = HUMAN;
+    return HUMAN;
   } 
   
   if (!Object.keys(WINS).includes(GAME.choices) && player1 !== player2) {
-    winner = COMPUTER;
+    return COMPUTER;
   } 
-
-  return winner;
 };
 
-const determineGameChampion = roundWinner => {
-  if (GAME.count > MAX_WINS && HUMAN.score !== COMPUTER.score && roundWinner !== 'tie') {
+const determineGameChampion = () => {
     if (HUMAN.score > COMPUTER.score) {
       GAME.champion = HUMAN.name;
     } else if (COMPUTER.score > HUMAN.score) {
       GAME.champion = COMPUTER.name;
     } 
-  }
-
-  return GAME.champion;
-};
-
-const displayWinner = winner => {
-  switch (winner) {
-    case HUMAN: 
-      return prompt(`${WEAPONS[HUMAN.choice]} ${WINS[GAME.choices]} ${WEAPONS[COMPUTER.choice]}!`);
-    case COMPUTER: 
-      return prompt(`${WEAPONS[COMPUTER.choice]} ${WINS[reverseChoices(HUMAN.choice, COMPUTER.choice)]} ${WEAPONS[HUMAN.choice]}`);
-    case 'tie': 
-      return prompt('IT IS A TIE.');
-    default: 
-      break;
-  }
+    
+    return GAME.champion;
 };
 
 const resetEverything = () => {
@@ -198,32 +179,57 @@ const playAgain = () => {
   }
 };
 
+const firstRoundGreeting = () => {
+  getName();
+  prompt(`Hi, ${HUMAN.name}!`);
+  prompt(greeting);
+  prompt(shortRules);
+};
+
+const greetAndShowRules = () => {
+  firstRoundGreeting();
+
+  if (readline.keyInYNStrict(`${gameRulesPrompt}`)){
+    console.clear();
+    prompt(`${longRules}`);
+  } else {
+    console.clear();
+  } 
+};
+
+const displayScore = gameState => `${gameState} SCORE IS ${HUMAN.name}: ${HUMAN.score} & COMPUTER: ${COMPUTER.score}.`; 
+
+const displayWinner = winner => {
+  switch (winner) {
+    case HUMAN: 
+      return prompt(`${WEAPONS[HUMAN.choice]} ${WINS[GAME.choices]} ${WEAPONS[COMPUTER.choice]}!`);
+    case COMPUTER: 
+      return prompt(`${WEAPONS[COMPUTER.choice]} ${WINS[reverseChoices(HUMAN.choice, COMPUTER.choice)]} ${WEAPONS[HUMAN.choice]}`);
+    case 'tie': 
+      return prompt('IT IS A TIE.');
+    default: 
+      break;
+  }
+};
+
+const displayGameChampion = champion => {
+  if (champion) {
+    prompt(`${champion} IS THE CHAMPION!`);
+  }
+};
+
 // Start Main Game Loop
 do {
-  resetChoices();
-
   if (GAME.count === 1 && !continueGame) {
-    getName();
-    prompt(`Hi, ${HUMAN.name}!`);
-    prompt(greeting);
-    prompt(shortRules);
-
-    if (readline.keyInYNStrict(`${gameRulesPrompt}`)){
-      console.clear();
-      prompt(`${longRules}`);
-    } else {
-      console.clear();
-      prompt(`GAME ${GAME.count} STARTS NOW`);
-    } 
-  }  
+    greetAndShowRules();
+  } 
+  
+  resetChoices();
+  prompt(`GAME ${GAME.count} STARTS NOW`); 
   
   let human = getHumanChoice();
   let computer = getComputerChoice();
   console.clear();
-
-  if (human.toLowerCase() === 'x') {
-    break;
-  }
 
   prompt(`YOU CHOSE ${WEAPONS[human]}. COMPUTER CHOSE ${WEAPONS[computer]}.`);
 
@@ -233,22 +239,19 @@ do {
   }
 
   displayWinner(currentWinner);
-  
-  if (currentWinner !== 'tie' && !GAME.champion) {
-    prompt(`${currentWinner.name} WINS ROUND ${GAME.count - 1}!`);
-  } 
 
-  resetChoices();
-  determineGameChampion(currentWinner);
-
-  if (GAME.champion) {
+  if (GAME.count > MAX_WINS && HUMAN.score !== COMPUTER.score && currentWinner !== 'tie') {
     GAME.state = 'FINAL';
-    prompt(`${GAME.state} SCORE IS ${HUMAN.name}: ${HUMAN.score} & COMPUTER: ${COMPUTER.score}.`);
-    prompt(`${GAME.champion} IS THE CHAMPION!`);
+    determineGameChampion(currentWinner);
+    displayGameChampion(GAME.champion, GAME.state);
+    prompt(displayScore(GAME.state));
     playAgain();
-  } else {
-    prompt(`${GAME.state} SCORE IS ${HUMAN.name}: ${HUMAN.score} & COMPUTER: ${COMPUTER.score}.`);
+  } else if (currentWinner !== 'tie' && !GAME.champion) {
+    prompt(`${currentWinner.name} WINS ROUND ${GAME.count - 1}!`);
+    prompt(displayScore(GAME.state));
   }
+
+  
 } while (GAME.play);
 
 console.clear();
